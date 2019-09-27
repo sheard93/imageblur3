e17410318a8b1acdc4bde001a92dfad6c76ceaef
sheard93 <shelbyzaglin@gmail.com>
2019-09-27 15:00:26 +0000

    Initial commit

---
+++ imageblur3.rb
@@ -0,0 +1,61 @@
+class Image
+  attr_accessor :array
+
+  def initialize(array)
+    @array = array
+  end
+
+  def blur(distance = 1)
+    distance.times do
+      transform
+    end
+  end
+
+  def output_image
+    @array.each {|item| puts "#{item.join()}"}
+  end
+
+  private
+
+  def transform
+  # length of one secondary array
+    width = array[0].length
+
+  # number of secondary arrays (rows)
+    height = array.length
+
+    ones_array = []
+
+    array.each_with_index do |row, y|
+      row.each_with_index do |value, x|
+        ones_array << [x, y] if value == 1
+      end
+    end
+
+    ones_array.each do |coordinate|
+      x, y = coordinate
+
+      array[y][x + 1] = 1 unless (x + 1 >= width)
+      array[y][x - 1] = 1 unless (x - 1 < 0)
+      array[y + 1][x] = 1 unless (y + 1 >= height)
+      array[y - 1][x] = 1 unless (y - 1 < 0)
+    end
+  end
+end
+
+
+image = Image.new([
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+])
+
+image.blur(3)
+image.output_image
